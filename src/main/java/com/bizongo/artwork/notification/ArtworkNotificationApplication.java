425248ffa08fc49fd6c02a25794508fc0d49d747
Ketan Khatwal <ketan.khatwal@bizongo.com>
2022-04-29 15:56:09 +0530

    Initial Commit

---
+++ src/main/java/com/bizongo/artwork/notification/ArtworkNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.bizongo.artwork.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtworkNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtworkNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizongo/artwork/notification/ArtworkNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizongo.artwork.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtworkNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
